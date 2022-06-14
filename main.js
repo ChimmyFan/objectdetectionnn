d50d84b6d6c400953a75456f020af15c46edbb9d
ChimmyFan <88235365+ChimmyFan@users.noreply.github.com>
2022-06-14 18:52:28 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 img="";
+status ="";
+objects =[];
 
 function preload()
 {
@@ -10,13 +12,50 @@ function setup()
 {
     canvas=createCanvas(640,420);
     canvas.center();
+    objectDetector =ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = ("Status:Detecting Objects");
+}
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error,results)
+{
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
 function draw()
 {
     image(img,0,0,640,420);
-    fill("#4169e1");
+
+    if(status !="")
+
+    for(i = 0; i<objects.length; i++)
+    {
+        document.getElementById("status").innerHTML="status:Object Detected";
+
+        fill("#4169e1");
+        percent=floor(objects[i].confidence*100);
+        text(objects[i].label+""+percent+"%",objects[i].x+15,objects[i].y+15);
+        noFill();
+        stroke("#30D5C8");
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+
+    }
+    fill("#");
     text("Dog",45,75);
     noFill();
     stroke("#4169e1");
     rect(30,60,450,350);
+
+    fill("#30D5C8");
+    text("Cat",320,120);
+    noFill();
+    stroke("#30D5C8")
+    rect(300,90,270,320)
 }
